7d76f610f49ec8960dace1f5095bdf5c5dc54d1e
Julie Kim <jewel2000k@yahoo.com>
2017-03-21 14:01:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ $(function() {
     var formMessages = $('#form-messages');
 
     // http://blog.teamtreehouse.com/create-ajax-contact-form
+    
+    $(form).submit(function(event) {
+    // Stop the browser from submitting the form.
+    event.preventDefault();
+        
+        
 });
