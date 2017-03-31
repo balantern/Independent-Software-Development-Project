b2b4a6863294fd523230016b7a634a077c6dc363
Julie Kim <jewel2000k@yahoo.com>
2017-03-31 12:44:16 -0500

    Update app.js

--- mailer/app.js
+++ mailer/app.js
@@ -3,46 +3,47 @@ $(function() {
     
     var formMessages = $('#form-messages');
 
-    // http://blog.teamtreehouse.com/create-ajax-contact-form
-    
     $(form).submit(function(event) {
-    // Stop the browser from submitting the form.
-    event.preventDefault();
+        // Stop the browser from submitting the form.
+        event.preventDefault();
     
-    var formData = $.ajax({
-        type: 'POST',
-        url: $(form).attr('action'),
-        data: formData
-    })
+        //serialize form data
+        var formData = $(form).serialize();
+        
+        //submit form using ajax
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
     
-    .done(function(response) {
-    // Make sure that the formMessages div has the 'success' class.
-        $(formMessages).removeClass('error');
-        $(formMessages).addClass('success');
+        .done(function(response) {
+        // Make sure that the formMessages div has the 'success' class.
+            $(formMessages).removeClass('error');
+            $(formMessages).addClass('success');
 
-    // Set the message text.
-        $(formMessages).text(response);
+        // Set the message text.
+            $(formMessages).text(response);
 
-    // Clear the form.
-        $('#name').val('');
-        $('#email').val('');
-        $('#message').val('');
-    })
+       // Clear the form.
+            $('#name').val('');
+            $('#email').val('');
+            $('#message').val('');
+        })
     
-    .fail(function(data) {
-    // Make sure that the formMessages div has the 'error' class.
-        $(formMessages).removeClass('success');
-        $(formMessages).addClass('error');
+        .fail(function(data) {
+        // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
 
-    // Set the message text.
-        if (data.responseText !== '') {
-            $(formMessages).text(data.responseText);
-        }
+        // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            }
         
-        else {
-        $(formMessages).text('An error occured and your message could not be sent.');
-        }
-    });
-        
-        
+            else {
+                $(formMessages).text('An error occured and your message could not be sent.');
+            }
+        });
+    }); 
 });
