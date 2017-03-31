f5ba680aad83b9d9d6c0dfe460a9c308d64089f0
Julie Kim <jewel2000k@yahoo.com>
2017-03-31 12:46:53 -0500

    Update mailer.php

--- mailer/mailer.php
+++ mailer/mailer.php
@@ -2,7 +2,7 @@
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = strip_tags(trim($_POST["name"]));
-    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+            $name = str_replace(array("\r","\n"),array(" "," "),$name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
     
